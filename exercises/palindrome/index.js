5e16248d72eb2147304e42bf6a6c56d9337fa10b
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-20 22:26:38 -0500

    Added a more efficient palindrome function that cut runtime in half

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -13,11 +13,29 @@
 
 //solve the issue of the double comparisons
 //Array.every is not the most efficient
-function palindrome(str){
-    return str.split("").every((char, i)=>{
-        console.log(char, str[str.length-i-1])
-        return char === str[str.length-i-1]
-    })
-}
+// function palindrome(str){
+    // return str.split("").every((char, i)=>{
+    //     console.log(char, str[str.length-i-1])
+    //     return char === str[str.length-i-1]
+    // })
+// }
 
+function palindrome(str){
+    // if length is even number just iterate through half comparisons
+    // if length is odd divide by two +1
+    let loopLength
+    if(str.length%2 == 0){
+        loopLength = str.length/2
+    }
+    else{
+        loopLength = str.length/2 + 1
+    }
+    let isMatching = true
+
+    for(let i = 0; i < loopLength && isMatching; i++){
+        (str[i] === str[str.length-i-1])? isMatching = true: isMatching = false
+    }
+    return isMatching
+
+}
 module.exports = palindrome;
