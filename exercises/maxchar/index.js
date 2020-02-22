01fbb10d78126f7927c2b8f0c372c9858d687576
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-22 13:38:27 -0500

    maxChar function cleaned up

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,8 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//changed for cases where more than one character appeared max num times
+
 function maxChar(str) {
     let numOfChars = {}
     let strArray = str.split("")
@@ -24,19 +26,13 @@ function maxChar(str) {
             max = key
         }
       }
-    console.log("max: " , max)
 
     arrayOfMax = []
     for(let key of Object.keys(numOfChars)){
         if(numOfChars[key] == numOfChars[max])
         arrayOfMax.push(key)
     }
-
-
     return arrayOfMax
-
-    //   return max
-
 }
 console.log(maxChar("cc11")) // [c,1]
 console.log(maxChar("3fftittt")) // [t]
