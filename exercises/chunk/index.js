4d55291dc3291b1a36d1eab8e9f070497fee7696
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-22 22:20:13 -0500

    Solved chunk function

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let chunked = []
+    for(let number of array){
+        const last = chunked[chunked.length-1]
+        if(!last || last.length === size){
+            chunked.push([number]) // originally had chunked.push(number) which is why .push didnt work on line 19
+        }
+        else{
+            last.push(number)
+        }
+    }
+    return chunked
+}
 
 module.exports = chunk;
