9f4ae1635892a2d98d88f9b99a4d2372a18f5def
Zermina Ejaz <zerminaejaz@gmail.com>
2020-02-23 20:07:26 -0500

    Solved Anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let strA = stringA.replace(/[^0-9a-z]/gi, '')
+    let strB = stringB.replace(/[^0-9a-z]/gi, '')
+    if(strA.length !== strB.length){
+        return false
+    }
+   
+    let mapA = createMap(strA)
+    let mapB = createMap(strB)
+
+    if(Object.keys(mapA).length !== Object.keys(mapB).length){
+        return false
+    }
+    else{
+        //then check deeper into the values to see if they all match
+        //while loop through one map and check to see if the keys match
+        for(let [key, value] of Object.entries(mapA)){
+            if(mapB[key] !== value)
+                return false
+        }
+        return true
+    }
+}
+
+function createMap(str){
+    let map = {}
+    for(let char of str){
+        if(map[char]){
+            map[char] += 1
+        }
+        else{
+            map[char] = 1
+        }
+    }
+    return map
+}
 
 module.exports = anagrams;
